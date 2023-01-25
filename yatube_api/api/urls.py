c61f9403b3a3d0aec84d7a5fcacbbbb5ad24a2fc
creamsandwch <neuronrus1996@gmail.com>
2023-01-25 15:53:29 +0500

    pre pull commit

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -4,14 +4,14 @@ from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, FollowViewSet
 
 
-router_v2 = DefaultRouter()
+router_v1 = DefaultRouter()
 
-router_v2.register(r'posts', PostViewSet)
-router_v2.register(r'posts\/(?P<post_pk>([1-9]\d*))\/comments', CommentViewSet)
-router_v2.register(r'follow', FollowViewSet)
+router_v1.register(r'posts', PostViewSet)
+router_v1.register(r'posts\/(?P<post_pk>([1-9]\d*))\/comments', CommentViewSet)
+router_v1.register(r'follow', FollowViewSet)
 
 
 urlpatterns = [
-    path('v1/', include(router_v2.urls)),
+    path('v1/', include(router_v1.urls)),
     path('auth/', include('djoser.urls.jwt')),
 ]
